093cd266a1e8f852f681f2e367f4cc0a49029c88
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2019-12-21 16:55:06 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 1, "M", "yellow","crazy", 0);
+insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 2, "F", "black","cool", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 3, "M", "gray","hot", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 4, "F", "normal","a", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 7, "M", "tan","loving", 0);
+insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 5, "M", "red","active", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 6, "M", "purple", "passive", 0);
+insert into bears (name, age, gender, color, temperament, alive) values (null, 9, "M", "white","loud", 1);
+
